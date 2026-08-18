48677488ca1af18ddd0b72a0c0f19eeebcd0b7b4
Lucas Gomes <lucasgomesvicent@gmail.com>
2026-08-18 20:40:25 -0300

    commit adicionando files

---
+++ index.php
@@ -0,0 +1,113 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo de Química</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet">
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+
+<header  class="bg-black shadow-lg" style="height: 80px" >
+    <ul class="list-unstyled text-white">
+        
+        <li class="text-center fs-1 " >
+            CARBONMONS
+        </li>
+    </ul>
+</header>
+
+<div class="bg-success " >
+
+        <h3 class="fs-4 text-center">BATALHA DOS HIDROCARBONETOS</h3> 
+
+        <ul class="list-unstyled d-flex justify-content-center gap-5 p-2">
+            <li class="item">
+            Alcanos
+            </li>
+
+            <li class="item">
+                Alcenos
+            </li>
+
+            <li class="item">
+                Alcinos
+            </li>
+
+            <li class="item">
+                Ciclos
+            </li>
+
+            <li class="item">
+                Aromáticos
+            </li>
+        </ul>
+    </div>
+    
+<div class="container-batalha" style="height: 400px">
+
+</div>
+
+
+<footer>
+    <hr>
+       <h3 class="text-center">
+            SUA COLEÇÃO
+       </h3>
+
+       <article class="card">
+           <h6>Grupo do elemento</h6>
+           <h6>Nome do elemento</h6>
+
+        <div>
+            <img src="" alt="">
+        </div>
+
+           <ul>
+                <li>Composição(c2h4)</li>
+                <li>raridade</li>
+            </ul>
+                <span>hp</span>
+            
+                <button>ataque 1</button>
+                <button>ataque 2</button>
+       </article>
+
+       
+    
+</footer>
+
+
+<style>
+    .item{
+        padding:10px;
+        color:white;
+        background-color: rgba(56, 190, 220, 0.5);        border-radius:20px;
+        transition: all ease 0.2s;
+    }
+
+    .item:hover{
+background-color: rgba(56, 190, 220, 0.28);    }
+
+    .card{
+        border-radius:20px;
+        height:350px;
+        width:300px;
+        margin-left:40px;
+        margin-top:-340px;
+        background-color:rgba(55, 128, 64, 0.5);
+    }
+</style>
+
+<script>
+</script>
+
+</body>
+</html>
